cc1b56c0ddd701bcdb5ece61aba489ccaa197ba6
Abinash Mohapatra <abinash@expensebit.com>
2020-09-28 20:59:59 +0530

    design productList and productDetails page

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,14 +3,22 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 
 import ProductOverViewScreen from '../screens/shop/ProductOverViewScreen';
+import ProductDetailsScreen from '../screens/shop/ProductDetailsScreen';
 import Colors from '../constants/Colors';
 
 const ProductNavigator = createStackNavigator({
-    ProductsOverView: ProductOverViewScreen
+    ProductsOverView: ProductOverViewScreen,
+    ProductDetail: ProductDetailsScreen
 }, {
     defaultNavigationOptions: {
         headerStyle: {
             backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTitleStyle: {
+            fontFamily: 'open-sans-bold'
+        },
+        headerBackTitleStyle: {
+            fontFamily: 'open-sans',
         },
         headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
     }
